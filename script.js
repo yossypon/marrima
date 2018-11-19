8dc53dd66d886c4966da0b7e835c92f259fab638
Yoshiki Sakai <fhknq111@hotmail.co.jp>
2018-11-19 11:57:59 +0900

    8日目　学校昼
    jQueryでリサイズ設定完了（gnavとスライドショー）

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 $(function(){
     $(window).scroll(function(){        
         if($(this).scrollTop() < 30){
-            $(".top_parts").css({"position": "static"});
+            $(".top_parts").css({"position": "relative"});
         }else{
             $(".top_parts").css({"position":"fixed","top":0});
         }
@@ -12,35 +12,70 @@ $(function(){
 
 
 // --- gnavの動き --------------------------------- //
-$(function(){
-    $(".gnav_2nd").hide();
-    
     // リサイズでやる
-    var w = $(window).width();
-    var x = 801; // 800pxがFireHD8の画面サイズ
-    if(w >= x){
-
-    $(".gnav_1st_stage > li").mouseenter(function(){
-        $("ul:not(:animated)",this).show();
-    }).mouseleave(function(){
-        $("ul",this).slideUp("fast");
+    $(window).on('load resize', function(){
+        $(".gnav_2nd").hide();
+        var w = $(window).width();
+        var x = 801; // 800pxがFireHD8の画面サイズ
+        if(w >= x){
+            $(".gnav_1st_stage > li").mouseenter(function(){
+                $("ul:not(:animated)",this).show();
+            }).mouseleave(function(){
+                $("ul",this).slideUp("fast");
+            })
+        }else{
+            $(".gnav_1st_stage > li").mouseenter(function(){
+                $("ul:not(:animated)",this).hide();
+        })
+        }
     })
 
-    }
+
+// --- member_menuの動き -------------------------------- //
+// --- ハンバーガーをクリックしたら１秒後にバツ印になる ---
+$(function(){
+    $(".head_face_hamburger_menu").click(function(){
+        $(".head_face_hamburger_menu .middle").css("background","rgba(51, 51, 51, 0)");
+        $(".head_face_hamburger_menu .middle").css("transition-delay","1s");
+        $(".head_face_hamburger_menu .top").css("transform","rotate(-45deg) translateY(0px)");
+        $(".head_face_hamburger_menu .top").css("transition-delay","1s");
+        $(".head_face_hamburger_menu .bottom").css("transform","rotate(45deg) translateY(0px)");
+        $(".head_face_hamburger_menu .bottom").css("transition-delay","1s");
+    })
+})
+
+// --- ハンバーガーをクリックしたら左からメニューを持ってくる ---
+$(function(){
+    $(".head_face_hamburger_menu").click(function(){
+        $(".top_parts").css("margin-left","280px"); // margin-rightはだめ（自分が動きたいときはmargin-left）
+        $(".top_parts").css("transition",".5s");
+
+    })
 })
 
 
-// --- new_itemのスライドショー （画像）--------------------- //
-$(function(){
-    $('.slideshow_pic').each(function(){
+    
 
-        $("img",this).each(function(){
-            $(this).css("margin-left",-1*(parseInt($(this).width()/2)));
-            //console.log(parseInt($(this).width()/2));
+
+// --- new_itemのスライドショー （画像）--------------------- //
+$(window).on('load resize', function(){
+    var timer = false;
+        if (timer !== false){
+            clearTimeout(timer);
+        }
+    timer = setTimeout(function(){
+        $(".slideshow_pic").each(function(){
+
+            $("img",this).each(function(){
+                $(this).css("margin-left",-1*(parseInt($(this).width()/2)));
+                //console.log(parseInt($(this).width()/2));
+            })
         })
-    })
+    },200);
+})
     // 上の方法で幅の半分の長さを取得しているが、パソコン画面でリサイズした際に値を新しく取得するわけではないので、自動で新しく値を取得させるには専用の記述が必要。（これはかなりCPU喰うらしい）スマホの画面、タブレットの画面などそれぞれの端末で開いた際の表示は問題ない。問題なのは、PCで閲覧中に画面をリサイズされた場合。
 
+$(window).on('load', function(){
 	var interval = 3500; 
 	$('.slideshow_pic').each(function(){
 
